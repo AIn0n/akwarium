5c09f0ac5cd1041ceb1518e1970cfd9a0b3c2619
LogicalInsanity <idkwtw223@gmail.com>
2022-10-20 22:01:56 +0200

    Login added

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from os import urandom
 
 app = Flask(__name__)
 
 # rationale for that kind of imports:
 # https://stackoverflow.com/questions/11994325/how-to-divide-flask-app-into-multiple-py-files
 import hello
+
+
+app.config["SECRET_KEY"] = urandom(24)
--- backend/hello.py
+++ backend/hello.py
@@ -1,12 +1,41 @@
 from app import app
-from flask import make_response, request
+from flask import make_response, request, redirect, url_for, abort
+from flask_login import LoginManager, login_user, UserMixin, login_required
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 i = 0
+
+
+class User(UserMixin):
+    id = "123"
 
 
 @app.route("/hello", methods=["GET"])
 def hello():
     global i
-    name = request.form["name"]
     i += 1
-    return make_response({"hello": name, "counter": i}, 200)
+    return make_response({"hello": "", "counter": i}, 200)
+
+
+def user_login(name, password):
+    if password == "qwerty":
+        return True
+    return False
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    name = request.form["name"]
+    password = request.form["password"]
+    user = User()
+    if user_login(name, password):
+        login_user(user)
+        return redirect(url_for("hello"))
+    return abort(418)
+
+@app.route("/logout")
+@login_required
+def logout():
+    #logout_user()
+    return redirect(url_for("login"))#TEMP
