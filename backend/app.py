e8d1de1526ac4bdd6de7b3ccbeeae24cb91b82c1
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-10-22 22:39:24 +0200

    added user authentication

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,12 @@
 from flask import Flask
-import json # rationale: for reading config.json file
+import json  # rationale: for reading config.json file
+import pymongo  # rationale:
 
 app = Flask(__name__)
-
 app.config.from_file("config.json", load=json.load)
+mongo_client = pymongo.MongoClient(app.config["MONGO_API"])
+users_db = mongo_client["database"]["users"]
 
 # rationale for that kind of imports:
 # https://stackoverflow.com/questions/11994325/how-to-divide-flask-app-into-multiple-py-files
 import log_in as log_in
-
-
-
--- backend/log_in.py
+++ backend/log_in.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app, users_db
 from flask import make_response, request, redirect, url_for, abort
 import flask_login as fl
 
@@ -8,14 +8,17 @@ login_manager = fl.LoginManager()
 login_manager.init_app(app)
 logged_users = set()
 
-#user class
+# user class
 class User(fl.UserMixin):
     def __init__(self, name) -> None:
         super().__init__()
-        id = name
+        self.id = name
+
 
 # TODO: remove!
 i = 0
+
+
 @app.route("/hello", methods=["GET"])
 def hello():
     global i
@@ -23,24 +26,25 @@ def hello():
     return make_response({"hello": "", "counter": i}, 200)
 
 
-def user_login(name, password):
-    if password == "qwerty":
-        return True
-    return False
+def verify_password(name, password):
+    x = users_db.find_one({"name": name})
+    print(x)
+    return password == x["password"]
 
 
 @app.route("/login", methods=["POST"])
 def login():
     name = request.form["name"]
     password = request.form["password"]
-    user = User()
-    if user_login(name, password):
+    user = User(name)
+    if verify_password(name, password):
         fl.login_user(user)
         return redirect(url_for("hello"))
     return abort(418)
 
+
 @app.route("/logout")
 @fl.login_required
 def logout():
-    #logout_user()
-    return redirect(url_for("login"))#TEMP
+    # logout_user()
+    return redirect(url_for("login"))  # TEMP
