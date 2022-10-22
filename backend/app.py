2e63607054741ef002abf213a977584d463cb940
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-10-22 21:49:42 +0200

    Minor changes

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
-from os import urandom
+import json # rationale: for reading config.json file
 
 app = Flask(__name__)
 
+app.config.from_file("config.json", load=json.load)
+
 # rationale for that kind of imports:
 # https://stackoverflow.com/questions/11994325/how-to-divide-flask-app-into-multiple-py-files
-import hello
+import log_in as log_in
 
 
-app.config["SECRET_KEY"] = urandom(24)
+
