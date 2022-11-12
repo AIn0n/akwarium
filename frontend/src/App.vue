90fb2000cff2a2e22d76cc3ffd4cc5c5c1686e76
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-11-12 18:00:31 +0100

    still working with login site

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,20 +2,49 @@
 </script>
 
 <template>
-  <div class="container">
-    <div class="col">
-      <h1>Login</h1>
-      <div class="input-group input-group-sm mb-3">
-        <span class="input-group-text" id="inputGroup-sizing-sm">login</span>
-        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+  <div class="d-flex">
+    <div class="row text-center">
+      <div class="col align-self-center">
+        <h1>Login</h1>
+        <div class="input-group input-group mb-3">
+          <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="Login">
+        </div>
+        <div class="input-group input-group mb-3">
+          <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="password">
+        </div>
+        <div class="btn-group">
+          <button type="button" class="btn btn-outline-primary">login</button>
+          <button type="button" class="btn btn-outline-secondary">przypomnij haslo</button>
+        </div>
       </div>
-      <div class="input-group input-group-sm mb-3">
-        <span class="input-group-text" id="inputGroup-sizing-sm">password</span>
-        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
-      </div>
-      <div class="btn-group">
-        <button type="button" class="btn btn-outline-primary">zaloguj sie</button>
-        <button type="button" class="btn btn-outline-secondary">przypomnij haslo</button>
+      <div class="col">
+        <h6>
+          Don't have account? Register today for free and join the biggest
+          base of fish lovers!
+        </h6>
+        <label class="form-label">Login</label>
+        <div class="input-group input-group mb-3">
+          <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="Login">
+        </div>
+        <label class="form-label">Insert password</label>
+        <div class="input-group input-group mb-3">
+          <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="password">
+        </div>
+        <label class="form-label">repeat password</label>
+        <div class="input-group input-group mb-3">
+          <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="retype password">
+        </div>
+        <label class="form-label">insert email</label>
+        <div class="input-group input-group mb-3">
+          <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="email">
+        </div>
+        <label class="form-label">repeat email</label>
+        <div class="input-group input-group mb-3">
+          <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="retype email">
+        </div>
+        <div class="btn-group">
+          <button type="button" class="btn btn-outline-primary">Create account</button>
+        </div>
       </div>
     </div>
   </div>
