4359019908f2133ddbd87ebd5cf022c6a00f2574
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-11-27 19:23:39 +0100

    Added dynamicly rendering aquariums

--- frontend/src/views/AquariumChooser.vue
+++ frontend/src/views/AquariumChooser.vue
@@ -1,34 +1,37 @@
 <script setup>
 import Navbar from '../components/Navbar.vue';
-import { onMounted } from 'vue';
+import { onBeforeMount, ref } from 'vue';
 import instance from '../configs/axios_instance';
 
-onMounted(()=>{
+let aquariums = ref();
+
+onBeforeMount(()=>{
   let result = instance.get('/aquarium')
-    .then(res => { console.log(res.data)});
+    .then(res => { aquariums.value = res.data; });
 });
 
 </script>
 
 <template>
 <Navbar/>
-<div class="fluid-container">
+<div class="d-grid gap-5">
   <!-- https://getbootstrap.com/docs/5.2/components/card/#grid-cards -->
-  <div class="card" style="width: 18rem;">
-    <img src="https://cdn.britannica.com/29/121829-050-911F77EC/freshwater-aquarium.jpg" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>  
-  </div>
-  <div class="card" style="width: 18rem;">
-    <img src="https://cdn.britannica.com/29/121829-050-911F77EC/freshwater-aquarium.jpg" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>  
-  </div>
+  <li v-for="aquarium in aquariums">
+    <div class="card text-center w-75 mx-auto">
+      <img src="https://cdn.britannica.com/29/121829-050-911F77EC/freshwater-aquarium.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">{{aquarium.name}}</h5>
+        <p class="card-text"></p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </li>
+  <div class="card text-center w-75 mx-auto">
+      <div class="card-body">
+        <h5 class="card-title">Create new aquarium</h5>
+        <p class="card-text"></p>
+        <a href="#" class="btn btn-primary">+</a>
+      </div>
+    </div>
 </div>
 </template>
