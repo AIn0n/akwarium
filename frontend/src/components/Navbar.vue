ed1001979000121f965c9001d8484cff7c0a89a6
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-12-06 18:41:03 +0100

    Few minor stylistic changes

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,29 +1,36 @@
 <script setup>
 import { useRouter } from 'vue-router';
 import instance from '../configs/axios_instance';
+import { useAquariumStore } from '../stores/aquarium';
 
 const router = useRouter();
+const aquariumStore = useAquariumStore();
 
 function logout() {
   let result = instance.post("/logout").then( res => { router.push("/") });
 }
 
+console.log(aquariumStore.isPicked);
 // why this function is even needed?
 // well, this string "/settings" makes some problems in vue
 // structure of bindings, so - don't try make it smaller again!
 function settings() { router.push("/settings") }
-
-function aquaLife() { router.push('/aquaLife')}
+function aquaLife() { router.push('/aquaLife') }
+function aquaMonitor() { router.push('/aquaMonitor') }
+function aquariumPicker() {
+  aquariumStore.aquarium = "";
+  router.push('/Aquariums')
+}
 </script>
 
 <template>
   <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#" @click="aquariumPicker">Navbar</a>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
+      <ul class="navbar-nav" v-if="aquariumStore.isPicked">
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#">aquaMonitor</a>
+          <a class="nav-link" aria-current="page" href="#" @click="aquaMonitor">aquaMonitor</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" @click="aquaLife">aquaLife</a>
