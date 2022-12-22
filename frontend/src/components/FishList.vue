0f53f3c295a1376937d89f568cb08a0ef1fdcb1e
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-12-22 16:46:27 +0100

    Added highlighting currently picked fish

--- frontend/src/components/FishList.vue
+++ frontend/src/components/FishList.vue
@@ -15,10 +15,10 @@ function pickFish(fish_name)
     router.push('/fishPreview')
 }
 </script>
-
+"list-group-item list-group-item-action"
 <template>
 <div class="list-group list-group-flush col-3">
-  <a href="#" class="list-group-item list-group-item-action" v-for="fish in props.fish_list" @click="pickFish(fish.name)">
+  <a :class="{'list-group-item': true, 'list-group-action': true, 'active': fish.name === pickedFishStore.name}" v-for="fish in props.fish_list" @click="pickFish(fish.name)" href="#">
     {{fish.name}}
       <span v-if="fish.issues.length > 0" class="position-absolute top-50 start-100 translate-middle badge rounded-pill bg-danger">
       {{fish.issues.length}}
