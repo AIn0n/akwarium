29d38c079d56fd9aea780ade73cccc01d88acb7d
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-12-29 23:08:53 +0100

    Connected waterParamSetter to the API

--- frontend/src/views/WaterParamSetter.vue
+++ frontend/src/views/WaterParamSetter.vue
@@ -1,8 +1,36 @@
 <script setup>
+import { useAquariumStore } from '../stores/aquarium';
+import { useAlertsStore } from '../stores/alerts';
+
 import instance from '../configs/axios_instance';
 import { useRouter } from 'vue-router';
+import { ref } from 'vue';
+
+const aquariumStore = useAquariumStore();
+const alertStore = useAlertsStore();
 
 const router = useRouter();
+
+const water = ref({
+  kh: 0,
+  gh: 0,
+  ph: 0,
+  no2: 0,
+  no3: 0
+});
+
+function add_new_stats(water)
+{
+  let input = { ...water, aquarium: aquariumStore.aquarium_name };
+  instance.post('/log-add', input)
+    .then((res)=>{
+      alertStore.set_success("successfully changed water parameters");
+    }).catch((e)=>{
+      alertStore.set_danger("cannot add water parameters, please try later");
+    })
+  router.push('/aquaMonitor');
+  console.log(input);
+}
 
 </script>
 
@@ -10,23 +38,11 @@ const router = useRouter();
 <template lang="pug">
 h4(class="display-6 text-center my-3") Add water parameters by hand
 div(class="row my-3 mx-3")
-  div(class="col")
-    label(for="khInput" class="form-label") KH 
-    input(type="number" class="form-control" id="khInput")
-  div(class="col")
-    label(for="ghInput" class="form-label") GH
-    input(type="number" class="form-control" id="ghInput")
-  div(class="col")
-    label(for="no3Input" class="form-label") NO3
-    input(type="number" class="form-control" id="no3Input")
-  div(class="col")
-    label(for="no2Input" class="form-label") NO2
-    input(type="number" class="form-control" id="no2Input")
-  div(class="col")
-    label(for="phInput" class="form-label") PH
-    input(type="number" class="form-control" id="phInput")
+  div(class="col" v-for="key in Object.keys(water)")
+    label(class="form-label") {{  key }}
+    input(type="number" class="form-control" v-model="water[key]")
 div(class="container my-3 text-center w-75")
   div(class="row")
-    button(class="btn btn-primary col mx-3") add new stats
+    button(class="btn btn-primary col mx-3" @click="add_new_stats(water)") add new stats
     button(class="btn btn-secondary col mx-3" @click="router.push('/aquaMonitor')") back to aquaMonitor
 </template>
