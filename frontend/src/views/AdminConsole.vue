d714e30138b8bdac28712bb9f997e2decd6f3364
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-12-29 18:45:28 +0100

    Added building right object for backend api in admin panel

--- frontend/src/views/AdminConsole.vue
+++ frontend/src/views/AdminConsole.vue
@@ -5,15 +5,18 @@ import { ref, onBeforeMount } from 'vue';
 import { useAlertsStore } from '../stores/alerts';
 
 const router = useRouter();
-const species = ref({});
 const alertsStore = useAlertsStore();
 
+const species = ref({});
+const name = ref("");
+const velocity = ref(0);
+
 const water_requirements = ref({
-  kh: {min: 0, max: 0},
-  gh: {min: 0, max: 0},
-  no3: {min: 0, max: 0},
-  no2: {min: 0, max: 0},
-  ph: {min: 0, max: 0}
+  KH: {min: 0, max: 0},
+  GH: {min: 0, max: 0},
+  NO3: {min: 0, max: 0},
+  NO2: {min: 0, max: 0},
+  pH: {min: 0, max: 0}
 });
 
 onBeforeMount(()=>{
@@ -39,24 +42,38 @@ function is_param_invalid(key, water_requirements)
           water_requirements[key].min < 0 || water_requirements[key].max < 0;
 }
 
+function add_new_specie(name, vel, water_requirements)
+{
+  const new_specie = {};
+  for (const [key, value] of Object.entries(water_requirements)) {
+    new_specie["min_" + key] = value.min;
+    new_specie["max_" + key] = value.max;
+  }
+  new_specie.name = name;
+  new_specie.required_size = vel;
+  console.log(new_specie);
+}
+
 </script>
 
 <template lang="pug">
-div(class="container row")
-  div(class="col-3 list-group")
-    div(class="list-group-item" v-for="specie in species")
-      a {{ specie }}
-      button(type="button" class="btn-close position-absolute start-100" aria-label="Close")
+div(class="row container")
+  div(class="col-3")
+    p(class="text-center fs-4 mt-3") remove species
+    div(class="ms-1 list-group container")
+      div(class="list-group-item" v-for="specie in species")
+        a {{ specie }}
+        button(type="button" class="btn-close position-absolute start-100" aria-label="Close")
   div(class="col container text-center")
     h3(class="display-6 mt-3") Admin panel
     div(class="row mx-3")
       div(class="col")
         div(class="input-group my-3")
           span(class="input-group-text") specie name 
-          input(type="text" class="form-control")
+          input(type="text" class="form-control" v-model="name")
         div(class="input-group my-3")
           span(class="input-group-text") required velocity
-          input(type="number" class="form-control")
+          input(type="number" class="form-control" v-model="velocity")
         h3(class="display-7") define water requirements
         table(class="table table-dark table-striped")
           thead
@@ -71,5 +88,7 @@ div(class="container row")
                 input(type="number" class="form-control form-control-sm" v-model="val.min")
               th
                 input(type="number" class="form-control form-control-sm" v-model="val.max")
-
+        div(class="container row align-center")
+          button(type="button" class="btn btn-danger col mx-3" @click="add_new_specie(name, velocity, water_requirements)") click me
+          button(type="button" class="btn btn-danger col mx-3" @click="router.push('/Settings')") back to settings
 </template>
