078f396e16c9d755a013e22156f6a44bc8e8ec1e
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2023-01-05 22:17:45 +0100

    watertable is finally working properly in aquamonitor

--- frontend/src/components/WaterTable.vue
+++ frontend/src/components/WaterTable.vue
@@ -1,15 +1,17 @@
 <script setup>
-const props = defineProps(['water', 'requirements'])
+const props = defineProps(['water', 'water_min', 'water_max'])
 
 function check_water_row(key)
 {
   const curr = props.water[key];
-  const min = props.requirements.water_min[key];
-  const max = props.requirements.water_max[key];
+  const min = props.water_min[key];
+  const max = props.water_max[key];
   console.log(curr, min, max);
+  const not_defined = min === "" && max === "";
   return {
-    'table-danger': min > curr || curr > max,
-    'table-warning': curr == min || curr == max
+    'table-danger': (min > curr || curr > max) && !not_defined,
+    'table-warning': (curr == min || curr == max) && !not_defined,
+    'table-info': not_defined
   };  
 }
 </script>
@@ -26,9 +28,9 @@ function check_water_row(key)
 <tbody>
   <tr v-for="(val, key) in props.water" :class="check_water_row(key)">
     <td>{{ key }}</td>
-    <td>{{ props.requirements.water_min[key]}}</td>
+    <td>{{ props.water_min[key]}}</td>
     <td>{{ val }}</td>
-    <td>{{ props.requirements.water_max[key]}}</td>
+    <td>{{ props.water_max[key]}}</td>
   </tr>
 </tbody>
 </template>
--- frontend/src/views/AquaMonitor.vue
+++ frontend/src/views/AquaMonitor.vue
@@ -42,22 +42,6 @@ const water = {
   NO2: 12,
   PH: 6
 };
-const water_requirements = {
-  water_min: {
-    KH: 11,
-    GH: 10,
-    NO3: 10,
-    NO2: 6,
-    PH: 3,
-  },
-  water_max: {
-    KH: 20,
-    GH: 20,
-    NO3: 15,
-    NO2: 15,
-    PH: 9
-  }
-}
 </script>
 
 <template>
@@ -69,7 +53,7 @@ const water_requirements = {
       <div class="row my-3">
         <img :src="aquariumStore.aquarium_object['image']" class="col-5 rounded mx-auto" alt="...">
         <table class="col table table-bordered table-striped table-hover mx-auto">
-          <WaterTable :water="aquariumStore.water_object" :requirements="water_requirements" />
+          <WaterTable :water="aquariumStore.water_object" :water_min="aquariumStore.aquarium_object.water_min" :water_max="aquariumStore.aquarium_object.water_max" />
         </table>
       </div>
       <AlertFromStore />
