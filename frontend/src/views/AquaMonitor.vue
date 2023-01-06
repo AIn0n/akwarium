73d6df045e6c8922bdf93b5eb1641b4fdaa127e0
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2023-01-06 19:58:49 +0100

    Added aquaMonitor

--- frontend/src/views/AquaMonitor.vue
+++ frontend/src/views/AquaMonitor.vue
@@ -19,14 +19,36 @@ const alertStore = useAlertsStore();
 const router = useRouter();
 const downloadLink = ref(null);
 
-onBeforeMount(()=>{
-   instance.get("aquarium/" + aquariumStore.aquarium_name)
+const devices = ref({});
+
+async function get_aquarium()
+{
+  const result = await instance.get("aquarium/" + aquariumStore.aquarium_name)
     .then((res)=>{
       aquariumStore.aquarium_object = res.data;
     }).catch((e)=> {
       alertStore.set_danger("cannot connect to the server " + e);
       router.push('/Aquariums');
     });
+}
+
+async function get_devices()
+{
+  const result = await instance.get('/devices').then((res)=>{
+    devices.value['filter'] = res.data['filter'].filter(x => x._id === aquariumStore.aquarium_object.filter_id)[0];
+    devices.value['heater'] = res.data['heater'].filter(x => x._id === aquariumStore.aquarium_object.heater_id)[0];
+    devices.value['light'] = res.data['light'].filter(x => x._id === aquariumStore.aquarium_object.lamp_id)[0];
+    devices.value['pump'] = res.data['pump'].filter(x => x._id === aquariumStore.aquarium_object.pump_id)[0];
+    console.log(devices.value)
+  }).catch((e)=>{
+    alertStore.set_danger("cannot get devices list");
+    router.push('/Aquariums');
+  })
+}
+
+onBeforeMount(()=>{
+  get_aquarium();
+  get_devices();
   instance.get('/log-newest/' + aquariumStore.aquarium_name)
     .then((res)=> {
       aquariumStore.water_object = Object.fromEntries(Object.entries(res.data.message).filter(([key]) => key != 'date'));
@@ -70,14 +92,30 @@ function import_file(event) {
         </table>
       </div>
       <AlertFromStore />
-      <div class="container text-center row">
+      <table class="table table-sm">
+        <thead>
+          <th>type</th>
+          <th>name</th>
+          <th>power</th>
+          <th>description</th>
+        </thead>
+        <tbody class="table-group-divider">
+          <tr v-for="category in Object.keys(devices)">
+            <th>{{ category }}</th>
+            <th>{{ devices[category].name }}</th>
+            <th>{{ devices[category].parameter }}</th>
+            <th>{{ devices[category].description }}</th>
+          </tr>
+        </tbody>
+      </table>
+      <div class="text-center row">
         <button type="button" class="btn btn-outline-dark col mx-3">Save logs</button>
         <button type="button" class="btn btn-outline-dark col mx-3" @click="router.push('/WaterParamSetter')">Update Water parameters</button>
         <a ref="downloadLink" class="btn btn-outline-dark col mx-3" download="myfile.txt" @click="download">export to txt file</a>
-      <div class="my-3">
-        <label for="formFile" class="form-label">import aquarium</label>
-        <input type="file" ref="fileInput" class="form-control" id="formFile" @change="import_file"/>
-      </div>
+        <div class="my-3">
+          <label for="formFile" class="form-label">import aquarium</label>
+          <input type="file" ref="fileInput" class="form-control" id="formFile" @change="import_file"/>
+        </div>
       </div>
     </div>
   </div>
